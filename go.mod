a9cfdb9841d3560b279926f9b38f4ac9147a9fe4
Yuki Kano <114910989+yuki-kano-lab@users.noreply.github.com>
2022-11-28 16:32:24 +0900

    Add: とりあえず軽く動くところまで

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/yuki-kano-lab/golang-cat
+
+go 1.19
+
+require github.com/alecthomas/chroma v0.10.0
+
+require github.com/dlclark/regexp2 v1.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/alecthomas/chroma"
+	"github.com/alecthomas/chroma/formatters"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
+)
+
+var (
+	theme    = flag.String("theme", "monokai", "highlights theme name")
+	language = flag.String("language", "", "file language")
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: golang-cat [options...] filepath")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	filename := flag.Arg(0)
+	fmt.Println(filename)
+	fmt.Println(*theme)
+	fmt.Println(*language)
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := printText(&bytes); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func printText(text *[]byte) (err error) {
+	var lexer chroma.Lexer
+	if *language != "" {
+		lexer = lexers.Get(*language)
+	} else {
+		lexer = lexers.Analyse(string(*text))
+	}
+	if lexer == nil {
+		lexer = lexers.Fallback
+	}
+
+	style := styles.Get(*theme)
+	if style == nil {
+		style = styles.Fallback
+	}
+
+	formatter := formatters.Get("terminal256")
+	if formatter == nil {
+		formatter = formatters.Fallback
+	}
+
+	iterator, err := lexer.Tokenise(nil, string(*text))
+	if err != nil {
+		return err
+	}
+
+	return formatter.Format(os.Stdout, style, iterator)
+}
